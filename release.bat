84fff446aba83d355c512b5af81487dffc1d3389
Timo Mehring <timo0mehring@gmail.com>
2026-09-02 20:09:08 +0200

    [Added] Windows TicTacToe.exe icon

---
+++ release.bat
@@ -0,0 +1,20 @@
+@echo off
+
+cd /d %~dp0
+
+if not exist release mkdir release
+
+g++ -std=c++17 -O2 -Wall -Wextra src/main.cpp src/playground.cpp src/gameplay.cpp icon.res -o release/TicTacToe.exe -lraylib -lopengl32 -lgdi32 -lwinmm
+
+if %errorlevel% neq 0 (
+    echo.
+    echo Release-Build fehlgeschlagen.
+    pause
+    exit /b %errorlevel%
+)
+
+echo.
+echo Release-Build erfolgreich.
+echo Datei: release\TicTacToe.exe
+
+pause
